ad91e2d49df2b9b9e1a5488a3d9b844e5751bcc5
fellow-baka <yotungh@gmail.com>
2019-07-30 18:17:39 +0200

    Fixes standa motor, module working now

--- hardware/motor/standa/standa.py
+++ hardware/motor/standa/standa.py
@@ -6,6 +6,8 @@ This module controls compact rotation stage 8MPR16-1 with the help of
 
 You have to have some ximc libraries from standa.
 
+80 steps = 1 degree of movement for 8MPR16-1.
+
 Qudi is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
@@ -45,6 +47,14 @@ from .pyximc import *
 
 
 class MotorStanda(Base, MotorInterface):
+    """
+    Module for 8SMC5-USB-B8-1 controller for 8MPR16-1 (and maybe others, not tested) stages sold by standa.
+    Using ximc libraries from standa.
+    Connected with USB interface.
+
+    Only non-network connected stages/controlles were tested.
+    Only single axis was tested.
+    """
 
     _modclass = 'standa_rotator'
     _modtype = 'hardware'
@@ -52,7 +62,11 @@ class MotorStanda(Base, MotorInterface):
     _libdir = ConfigOption('ximc_path',
                            'C:\\Users\\Eight\\PycharmProjects\\standa\\ximc-2.10.5\\ximc\\win64',
                            missing='error')
-    # os.environ["Path"] = _libdir + ";" + os.environ["Path"]  # add dll
+
+    _axis_label = ConfigOption('axis_label', 'phi', missing='error')
+
+    _conv_factor = 80  # convert from steps to degrees
+    _refresh_interval_ms = 20  # refresh interval in milliseconds used for wait_for_stop method
 
     _device_id = None
     _open_name = None
@@ -61,6 +75,185 @@ class MotorStanda(Base, MotorInterface):
         """
         Connect to and initialize controller/motor.
         """
+        devenum = lib.enumerate_devices(EnumerateFlags.ENUMERATE_PROBE)  # only non-network connected
+        self._open_name = lib.get_device_name(devenum, 0)  # 0 == first device
+
+        self.log.info(f'{lib.get_device_count(devenum)} motors are found.')
+
+        self._device_id = lib.open_device(self._open_name)
+        print(self._device_id)
+
+    def on_deactivate(self):
+        """
+        Close device connection.
+        """
+        lib.close_device(byref(cast(self._device_id, POINTER(c_int))))
+
+    def get_constraints(self):
+        pass
+
+    def move_rel(self, param_dict):
+        """ Moves stage in given direction (relative movement) for asked amount of full steps.
+
+        @param dict param_dict: dictionary, which passes all the relevant
+                                parameters, which should be changed. Usage:
+                                 {'axis_label': <the-abs-pos-value>}.
+                                 'axis_label' must correspond to a label given
+                                 to one of the axis.
+
+        A smart idea would be to ask the position after the movement.
+
+        @return int: error code (0:OK, -1:error)
+        """
+        if self._axis_label in param_dict:
+            rel = param_dict[self._axis_label]
+            lib.command_movr(self._device_id, int(rel*self._conv_factor), 0)
+            self.wait_until_stop()
+            pos = self.get_pos()
+            return {self._axis_label: pos}
+        return -1
+
+    def move_abs(self, param_dict):
+        """ Moves stage to absolute position (absolute movement)
+
+        @param dict param_dict: dictionary, which passes all the relevant
+                                parameters, which should be changed. Usage:
+                                 {'axis_label': <the-abs-pos-value>}.
+                                 'axis_label' must correspond to a label given
+                                 to one of the axis.
+
+        @return int: error code (0:OK, -1:error)
+        """
+        if self._axis_label in param_dict:
+            abs = param_dict[self._axis_label]
+            lib.command_move(self._device_id, int(abs * self._conv_factor), 0)
+            self.wait_until_stop()
+            pos = self.get_pos()
+            return {self._axis_label: pos}
+        return -1
+
+    def abort(self, param_dict=None):
+        """ Stops (soft stop) movement of the stage and gives position
+
+        @return int: error code (0:OK, -1:error)
+        """
+        if self._axis_label in param_dict:
+            lib.command_sstp(self._device_id)
+            pos = self.get_pos()
+            return {self._axis_label: pos}
+        return -1
+
+    def get_pos(self, param_list=None):
+        """ Gets current position (in degrees) of the stage arms
+
+        @param list param_list: optional, if a specific position of an axis
+                                is desired, then the labels of the needed
+                                axis should be passed in the param_list.
+                                If nothing is passed, then from each axis the
+                                position is asked.
+
+        @return dict: with keys being the axis labels and item the current
+                      position.
+        """
+        x_pos = get_position_t()
+        result = lib.get_position(self._device_id, byref(x_pos))
+        if result == Result.Ok:
+            return x_pos.Position / self._conv_factor
+        else:
+            pass
+
+    def get_status(self, param_list=None):
+        """ Get the status of the position
+
+        @param list param_list: optional, if a specific status of an axis
+                                is desired, then the labels of the needed
+                                axis should be passed in the param_list.
+                                If nothing is passed, then from each axis the
+                                status is asked.
+
+        @return dict: with the axis label as key and the status number as item.
+        """
+        x_status = status_t()
+        result = lib.get_status(self._device_id, byref(x_status))
+        print("Result: " + repr(result))
+        if result == Result.Ok:
+            print("Status.Ipwr: " + repr(x_status.Ipwr))
+            print("Status.Upwr: " + repr(x_status.Upwr))
+            print("Status.Iusb: " + repr(x_status.Iusb))
+            print("Status.Flags: " + repr(hex(x_status.Flags)))
+
+        return [x_status.Ipwr, x_status.Upwr, x_status.Iusb, x_status.Flags, x_status.MoveSts]
+
+    def calibrate(self, param_dict=None):
+        """ Calibrates the stage.
+
+        @param dict param_dict: param_list: optional, if a specific calibration
+                                of an axis is desired, then the labels of the
+                                needed axis should be passed in the param_list.
+                                If nothing is passed, then all connected axis
+                                will be calibrated.
+
+        @return int: error code (0:OK, -1:error)
+
+        After calibration the stage moves to home position which will be the
+        zero point for the passed axis. The calibration procedure will be
+        different for each stage.
+        """
+        # if self._axis_label in param_dict:
+        lib.command_zero(self._device_id)
+        # return -1
+
+    def get_velocity(self, param_list=None):
+        """ Gets the current velocity for all connected axes.
+
+        @param dict param_list: optional, if a specific velocity of an axis
+                                is desired, then the labels of the needed
+                                axis should be passed as the param_list.
+                                If nothing is passed, then from each axis the
+                                velocity is asked.
+
+        @return dict : with the axis label as key and the velocity as item.
+        """
+        # Create move settings structure
+        mvst = move_settings_t()
+        # Get current move settings from controller
+        result = lib.get_move_settings(self._device_id, byref(mvst))
+        # Print command return status. It will be 0 if all is OK
+        return mvst.Speed
+
+    def set_velocity(self, param_dict):
+        """ Write new value for velocity.
+
+        @param dict param_dict: dictionary, which passes all the relevant
+                                parameters, which should be changed. Usage:
+                                 {'axis_label': <the-velocity-value>}.
+                                 'axis_label' must correspond to a label given
+                                 to one of the axis.
+
+        @return int: error code (0:OK, -1:error)
+        """
+        if self._axis_label in param_dict:
+            mvst = move_settings_t()
+            mvst.Speed = int(param_dict[self._axis_label])
+            lib.set_move_settings(self._device_id, byref(mvst))
+            return {self._axis_label: mvst.Speed}
+        return -1
+
+    def wait_until_stop(self):
+        """
+        Waits until movement is finished.
+        """
+        lib.command_wait_for_stop(self._device_id, self._refresh_interval_ms)
+
+    def baka(self):
+        x_status = edges_settings_t()
+        result = lib.get_status(self._device_id, byref(x_status))
+        print("Result: " + repr(result))
+        return [x_status.BorderFlags, x_status.EnderFlags, x_status.LeftBorder, x_status.RightBorder]
+
+    def get_info(self):
+        """Prints bunch of information about library, controller, and stage."""
+
         sbuf = create_string_buffer(64)
         lib.ximc_version(sbuf)
         print("Library version: " + sbuf.raw.decode())
@@ -87,164 +280,33 @@ class MotorStanda(Base, MotorInterface):
         if type(self._open_name) is str:
             self._open_name = self._open_name.encode()
 
-        self._device_id = lib.open_device(self._open_name)
-        print(self._device_id)
+    def test_set_microstep_mode_full(self):
+        print("\nSet microstep mode to 256")
+        # Create engine settings structure
+        eng = engine_settings_t()
+        # Get current engine settings from controller
+        result = lib.get_engine_settings(self._device_id, byref(eng))
+        # Print command return status. It will be 0 if all is OK
+        print("Read command result: " + repr(result))
+        # Change MicrostepMode parameter to MICROSTEP_MODE_FRAC_256
+        # (use MICROSTEP_MODE_FRAC_128, MICROSTEP_MODE_FRAC_64 ... for other microstep modes)
+        eng.MicrostepMode = MicrostepMode.MICROSTEP_MODE_FULL
+        # Write new engine settings to controller
+        result = lib.set_engine_settings(self._device_id, byref(eng))
+        # Print command return status. It will be 0 if all is OK
+        print("Write command result: " + repr(result))
 
-    def on_deactivate(self):
-        lib.close_device(byref(cast(self._device_id, POINTER(c_int))))
-
-    def get_constraints(self):
-        pass
-
-    def move_rel(self, param_dict):
-        """ Moves stage in given direction (relative movement)
-
-        @param dict param_dict: dictionary, which passes all the relevant
-                                parameters, which should be changed. Usage:
-                                 {'axis_label': <the-abs-pos-value>}.
-                                 'axis_label' must correspond to a label given
-                                 to one of the axis.
-
-        A smart idea would be to ask the position after the movement.
-
-        @return int: error code (0:OK, -1:error)
-        """
-        pass
-
-    def move_abs(self, param_dict):
-        """ Moves stage to absolute position (absolute movement)
-
-        @param dict param_dict: dictionary, which passes all the relevant
-                                parameters, which should be changed. Usage:
-                                 {'axis_label': <the-abs-pos-value>}.
-                                 'axis_label' must correspond to a label given
-                                 to one of the axis.
-
-        @return int: error code (0:OK, -1:error)
-        """
-        pass
-
-    def abort(self):
-        """ Stops movement of the stage
-
-        @return int: error code (0:OK, -1:error)
-        """
-        pass
-
-    def get_pos(self, param_list=None):
-        """ Gets current position of the stage arms
-
-        @param list param_list: optional, if a specific position of an axis
-                                is desired, then the labels of the needed
-                                axis should be passed in the param_list.
-                                If nothing is passed, then from each axis the
-                                position is asked.
-
-        @return dict: with keys being the axis labels and item the current
-                      position.
-        """
-        x_pos = get_position_t()
-        result = lib.get_position(self._device_id, byref(x_pos))
-        if result == Result.Ok:
-            return [x_pos.Position, x_pos.uPosition]
-        else:
-            pass
-
-    def get_status(self, param_list=None):
-        """ Get the status of the position
-
-        @param list param_list: optional, if a specific status of an axis
-                                is desired, then the labels of the needed
-                                axis should be passed in the param_list.
-                                If nothing is passed, then from each axis the
-                                status is asked.
-
-        @return dict: with the axis label as key and the status number as item.
-        """
-        pass
-
-    def calibrate(self, param_list=None):
-        """ Calibrates the stage.
-
-        @param dict param_list: param_list: optional, if a specific calibration
-                                of an axis is desired, then the labels of the
-                                needed axis should be passed in the param_list.
-                                If nothing is passed, then all connected axis
-                                will be calibrated.
-
-        @return int: error code (0:OK, -1:error)
-
-        After calibration the stage moves to home position which will be the
-        zero point for the passed axis. The calibration procedure will be
-        different for each stage.
-        """
-        pass
-
-    def get_velocity(self, param_list=None):
-        """ Gets the current velocity for all connected axes.
-
-        @param dict param_list: optional, if a specific velocity of an axis
-                                is desired, then the labels of the needed
-                                axis should be passed as the param_list.
-                                If nothing is passed, then from each axis the
-                                velocity is asked.
-
-        @return dict : with the axis label as key and the velocity as item.
-        """
-        pass
-
-    def set_velocity(self, param_dict):
-        """ Write new value for velocity.
-
-        @param dict param_dict: dictionary, which passes all the relevant
-                                parameters, which should be changed. Usage:
-                                 {'axis_label': <the-velocity-value>}.
-                                 'axis_label' must correspond to a label given
-                                 to one of the axis.
-
-        @return int: error code (0:OK, -1:error)
-        """
-        pass
-
-# sbuf = create_string_buffer(64)
-# lib.ximc_version(sbuf)
-# print("Library version: " + sbuf.raw.decode())
-#
-# devenum = lib.enumerate_devices(EnumerateFlags.ENUMERATE_PROBE)  # only non-network connected
-# print("Device enum handle: " + repr(devenum))
-# print("Device enum handle type: " + repr(type(devenum)))
-#
-# dev_count = lib.get_device_count(devenum)
-# print("Device count: " + repr(dev_count))
-#
-# controller_name = controller_name_t()
-# for dev_ind in range(0, dev_count):
-#     enum_name = lib.get_device_name(devenum, dev_ind)
-#     result = lib.get_enumerate_device_controller_name(devenum, dev_ind, byref(controller_name))
-#     if result == Result.Ok:
-#         print("Enumerated device #{} name (port name): ".format(dev_ind) + repr(enum_name) + ". Friendly name: " + repr(controller_name.ControllerName) + ".")
-#
-# open_name = None
-# open_name = lib.get_device_name(devenum, 0)
-#
-# print(open_name)
-#
-# if type(open_name) is str:
-#     open_name = open_name.encode()
-#
-# print(open_name)
-#
-# device_id = lib.open_device(open_name)
-#
-# print(device_id)
-#
-# print("\nRead position")
-# x_pos = get_position_t()
-# result = lib.get_position(device_id, byref(x_pos))
-# print("Result: " + repr(result))
-# if result == Result.Ok:
-#     print("Position: {0} steps, {1} microsteps".format(x_pos.Position, x_pos.uPosition))
-# print(x_pos.Position, x_pos.uPosition)
-# print(x_pos.Position)
-
-# lib.command_move(device_id, 2000, 0)
+    def get_mode(self):
+        # Create engine settings structure
+        eng = engine_settings_t()
+        # Get current engine settings from controller
+        result = lib.get_engine_settings(self._device_id, byref(eng))
+        # Print command return status. It will be 0 if all is OK
+        print("Read command result: " + repr(eng.MicrostepMode))
+        # # Change MicrostepMode parameter to MICROSTEP_MODE_FRAC_256
+        # # (use MICROSTEP_MODE_FRAC_128, MICROSTEP_MODE_FRAC_64 ... for other microstep modes)
+        # eng.MicrostepMode = MicrostepMode.MICROSTEP_MODE_FULL
+        # # Write new engine settings to controller
+        # result = lib.set_engine_settings(self._device_id, byref(eng))
+        # # Print command return status. It will be 0 if all is OK
+        # print("Write command result: " + repr(result))
